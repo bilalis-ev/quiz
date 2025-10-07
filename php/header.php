57bd1fde1a1fe218aa0fbd26877daf9b315da699
Evangelos Bilalis <bilalis.ev@gmail.com>
2025-10-07 22:47:28 +0300

    minor changes

--- php/header.php
+++ php/header.php
@@ -14,7 +14,7 @@
     <header>    
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">            
             <div class="container-fluid">
-                <a class="navbar-brand" href="index.php"><img src="../assets/logo.png" alt="Quizwars"></a>
+                <a class="navbar-brand" href="index.php"><img src="../assets/logo1.png" alt="Quizwars" height="40"></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
